3b8d633bd9ffd63c546391eaa8f86e06ec8f18a8
Brandon Dement <bdement@brightroll.com>
2015-07-07 14:23:52 -0700

    added culvers campaign

---
+++ campaigns/culvers/culvers.js
@@ -0,0 +1,44 @@
+var locations = require('./locations.json'); // MUST CONTAIN latitude, longitude fields
+var maxmind = require('maxmind');
+maxmind.init('/usr/local/share/GeoIP/GeoIPCity.dat');
+
+// const TEST_IP = '76.245.7.147'; // Illinois,
+// const TEST_IP = '71.10.122.95'; // Wisconsin,
+// const TEST_IP = '129.186.8.0'; // Iowa,
+const TEST_IP = '136.181.146.0'; // Michigan
+
+module.exports.onRequest = function(request, response) {
+	var ip = getIP(request);
+	if (!ip) {
+		throw new Error("Couldn't determine ip! (" + ip + ")");
+	}
+	var customerLocation = maxmind.getLocation(ip);
+	var closestDistance =  Number.MAX_VALUE;
+	var closestLocation = null;
+	for (var index in locations) {
+		var location = locations[index];
+		var distance = customerLocation.distance(location);
+		location.distance = distance;
+		if (distance < closestDistance) {
+			closestDistance = distance;
+			closestLocation = location;
+		}
+	}
+	if (closestLocation) {
+		response.write(JSON.stringify(closestLocation));
+		response.end();	
+	} else {
+		throw new Error("Couldn't find a location! (" + ip + ")");
+	}
+}
+function getIP(request) {
+	var socket = request.connection;
+	var family = socket.remoteFamily;
+	var address = socket.remoteAddress;
+	if (family == "IPv6") {
+		return address != '::1' ? address : TEST_IP;
+	} else if(family == "IPv4") {
+		return address != '127.0.0.1' ? address : TEST_IP;
+	}
+	return address;
+}
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var app = require('express')();
 var util 	= require('util');
 
 var whitelist = [
-	"/campaigns/ccanotherperiod/"
+	"/campaigns/ccanotherperiod/",
+	"/campaigns/culvers/"
 ]
 
 var handlers = {};
